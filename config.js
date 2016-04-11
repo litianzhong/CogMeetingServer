a19554b51fe69ef227d0a0cf9d63232f7782b0b2
litianzhong <litianzhong@qq.com>
2016-04-11 15:55:06 +0800

    modify config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
     port: 8080,
     host: '0.0.0.0',
     isHTTPS: false,
-    db: "mongodb://localhost:27017/ibmessager"
+    db: "mongodb://localhost:27017/cogmeeting"
 };
 
 module.exports = config;
